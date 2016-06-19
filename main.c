875428f986f03f6bc5adfcbb3e57c9485d9f03a4
Aaron Siegel <siegelaaron94@gmail.com>
2016-06-18 17:35:22 -0700

    added vector space modulation.

--- main.c
+++ main.c
@@ -67,7 +67,7 @@ void pid_setup(struct PID_data *data,float kp,float ki,float kd) {
 float pid_step(struct PID_data *data,float actual,float target) {
     //for tuning Ziegler?Nichols method
     //https://en.wikipedia.org/wiki/PID_controller
-    
+
     float error = actual - target;
     data->integral_error += error;
     data->delta_error = error - data->error;
@@ -75,6 +75,56 @@ float pid_step(struct PID_data *data,float actual,float target) {
     return data->kp * data->error + (data->ki * data->integral_error) + (data->kd * data->delta_error);
 }
 
+void write_space_vector_modulation(float theta,float a,float b,float c) {
+    float T1,T2,A,B,C;
+    if(theta >= 5.0f*PI/3.0f) { // Sector 6
+        T1 = -b;
+        T2 = a;
+        A = T1+T2;
+        B = 0;
+        C = T1;
+    }
+    else if(theta >= 4.0f*PI/3.0f) { // Sector 5
+        T1 = -c;
+        T2 = -a;
+        A = T2;
+        B = 0;
+        C = T1+T2;
+    }
+    else if(theta >= PI) { // Sector 4
+        T1 = c;
+        T2 = -b;
+        A = 0;
+        B = T1;
+        C = T1+T2;
+    }
+    else if(theta >= 2.0f*PI/3.0f) { // Sector 3
+        T1 = -a;
+        T2 = b;
+        A = 0;
+        B = T1+T2;
+        C = T2;
+    }
+    else if(theta >= PI/3.0f) { // Sector 2
+        T1 = a;
+        T2 = c;
+        A = T1;
+        B = T1+T2;
+        C = 0;
+    }
+    else {  // Sector 1
+        T1 = b;
+        T2 = -c;
+        A = T1+T2;
+        B = T2;
+        C = 0;
+    }
+    
+    // TODO check that A,B, and C are always positive.
+    set_a_duty_cycle(PERIOD * A);
+    set_b_duty_cycle(PERIOD * B);
+    set_c_duty_cycle(PERIOD * C);
+}
 
 #define Kp 0
 #define Ki 0
@@ -87,7 +137,7 @@ static float commanded_q_current = 0;
 
 static struct PID_data d_current_pid;
 static struct PID_data q_current_pid;
-        
+
 int main()
 {
     SYSTEM_Initialize();
@@ -105,44 +155,48 @@ int main()
     // Set UART RX to interrupt level 5
     struct l_irqmask irqmask = { 5, 5 };
     l_sys_irq_restore(irqmask);
-    
+
     pid_setup(&d_current_pid,Kp,Ki,Kd);
     pid_setup(&q_current_pid,Kp,Ki,Kd);
-    
+
     while (1) {
-        in_theta = PI*SPI2_Exchange16bit(0xFFFF)/180.0f;
+        TEST_PIN_Toggle();
+        __delay_ms(10);
+        /*in_theta = PI*SPI2_Exchange16bit(0xFFFF)/180.0f;
+
+        float sin_theta = sinf(in_theta);
+        float cos_theta = cosf(in_theta);
+
+        float alpha,beta;
+        clarke_transform(in_i_a,in_i_b,&alpha,&beta);
+
+        float in_i_d,in_i_q;
+        park_transform(alpha,beta,sin_theta,cos_theta,&in_i_d,&in_i_q);
+
+        float out_i_d = pid_step(&d_current_pid,in_i_d,0);
+        float out_i_q = pid_step(&q_current_pid,in_i_q,commanded_q_current);
+
+        inverse_park_transform(out_i_d,out_i_q,sin_theta,cos_theta,&alpha,&beta);
+
+        float out_i_a,out_i_b,out_i_c;
+        inverse_clarke_transform(alpha,beta,&out_i_a,&out_i_b,&out_i_c);
+        
+        write_space_vector_modulation(in_theta,out_i_a,out_i_b,out_i_c);*/
     }
 
     return -1;
 }
 
 void TMR5_CallBack() {
-    // TODO get the angle,and currents.
-    in_i_a = .25; // TODO read real value
-    in_i_b = .25; // TODO read real value
-    commanded_q_current = .8; // TODO read real value
+    //TEST_PIN_Toggle();
+}
 
-    float sin_theta = sinf(in_theta);
-    float cos_theta = cosf(in_theta);
-
-    float alpha,beta;
-    clarke_transform(in_i_a,in_i_b,&alpha,&beta);
-
-    float in_i_d,in_i_q;
-    park_transform(alpha,beta,sin_theta,cos_theta,&in_i_d,&in_i_q);
-
-    float out_i_d = pid_step(&d_current_pid,in_i_d,0);
-    float out_i_q = pid_step(&q_current_pid,in_i_q,commanded_q_current);
-
-    inverse_park_transform(out_i_d,out_i_q,sin_theta,cos_theta,&alpha,&beta);
-
-    float out_i_a,out_i_b,out_i_c;
-    inverse_clarke_transform(alpha,beta,&out_i_a,&out_i_b,&out_i_c);
-
-    // TODO write out_i_a,out_i_b,out_i_c
-    //set_a_duty_cycle(out_i_a);
-    //set_b_duty_cycle(out_i_b);
-    //set_c_duty_cycle(out_i_c);
+void __attribute__((interrupt, auto_psv)) _AD1Interrupt()
+{
+    IFS0bits.AD1IF = false;
+    in_i_a = ADC1BUF0/1024.0f; // TODO read real value
+    in_i_b = ADC1BUF1/1024.0f; // TODO read real value
+    commanded_q_current = ADC1BUF2/1024.0f; // TODO read real value
 }
 
 struct l_irqmask l_sys_irq_disable()
