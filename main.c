e6efe82e48dab8411e2b7bdc9e447af7aa04d338
Aaron Siegel <siegelaaron94@gmail.com>
2016-07-14 13:22:29 -0700

    Cleanup.

--- main.c
+++ main.c
@@ -49,27 +49,6 @@ int main()
     return -1;
 }
 
-struct l_irqmask l_sys_irq_disable()
-{
-    IEC0bits.U1RXIE = 0;
-    IEC0bits.U1TXIE = 0;
-    IFS0bits.U1TXIF = 0;
-    IFS0bits.U1RXIF = 0;
-    struct l_irqmask mask = { IPC2bits.U1RXIP, IPC3bits.U1TXIP };
-    return mask;
-}
-
-void l_sys_irq_restore(struct l_irqmask previous)
-{
-    IPC2bits.U1RXIP = previous.rx_level;
-    IFS0bits.U1TXIF = 0;
-    IEC0bits.U1RXIE = 1;
-
-    IPC3bits.U1TXIP = previous.tx_level;
-    IFS0bits.U1RXIF = 0;
-    IEC0bits.U1TXIE = 1;
-}
-
 void __attribute__((interrupt, auto_psv)) _PWMSpEventMatchInterrupt() {
     if(IFS3bits.PSEMIF) {
         AD1CON1bits.SAMP = 1; // Start sampling
@@ -131,4 +110,25 @@ void __attribute__((interrupt, auto_psv)) _PWMSpEventMatchInterrupt() {
         
         IFS3bits.PSEMIF = 0;
     }
+}
+
+struct l_irqmask l_sys_irq_disable()
+{
+    IEC0bits.U1RXIE = 0;
+    IEC0bits.U1TXIE = 0;
+    IFS0bits.U1TXIF = 0;
+    IFS0bits.U1RXIF = 0;
+    struct l_irqmask mask = { IPC2bits.U1RXIP, IPC3bits.U1TXIP };
+    return mask;
+}
+
+void l_sys_irq_restore(struct l_irqmask previous)
+{
+    IPC2bits.U1RXIP = previous.rx_level;
+    IFS0bits.U1TXIF = 0;
+    IEC0bits.U1RXIE = 1;
+
+    IPC3bits.U1TXIP = previous.tx_level;
+    IFS0bits.U1RXIF = 0;
+    IEC0bits.U1TXIE = 1;
 }
